9ef90279be9fe578fa150ae8e8a1d26d8579f2c7
Eryk Maushagen <eryk.maushagen@gmail.com>
2025-11-01 17:31:11 +0100

    3.1.1 Update

--- Blocks/Block_A/Blatt 03/blatt03/3.1.1/ha_tb.vhdl
+++ Blocks/Block_A/Blatt 03/blatt03/3.1.1/ha_tb.vhdl
@@ -21,12 +21,22 @@ begin
   process begin
     a <= '0';
     b <= '0';
+  
+  wait for 10 ns;
 -- ADD MORE TEST CASES BELOW
 -- Weitere Testfälle einfügen
---
---
---
+    a <= '0';
+    b <= '1';
 
+    wait for 10 ns;
+    a <= '1';
+    b <= '0';
+
+    wait for 10 ns;
+    a <= '1';
+    b <= '1';
+    wait for 10 ns;
+    assert false report "End of test";
     wait;
   end process;
 end test;
