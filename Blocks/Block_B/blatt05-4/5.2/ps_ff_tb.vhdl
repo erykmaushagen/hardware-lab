b07ff2c09a56aa72f70ca3c09c727446cdb2eccb
erykmaushagen <eryk.maushagen@gmail.com>
2025-11-22 13:50:06 +0100

    5.2.b ready

--- Blocks/Block_B/blatt05-4/5.2/ps_ff_tb.vhdl
+++ Blocks/Block_B/blatt05-4/5.2/ps_ff_tb.vhdl
@@ -1,5 +1,5 @@
 library ieee;
-  use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all;
 
 entity ps_ff_tb is
 end entity;
@@ -9,53 +9,54 @@ architecture testbench of ps_ff_tb is
   -- Baustein-Deklaration
   component ps_ff is
     port (
-      d, clk   : in  std_logic;
+      d, clk   : in std_logic;
       Q, not_Q : out std_logic
     );
   end component;
 
   -- Signaldeklaration
-  signal d_tb, clk_tb   : std_logic;
-  signal Q_tb, not_Q_tb : std_logic;
+  signal d, clk   : std_logic;
+  signal Q, not_Q : std_logic;
 
 begin
   -- Instanziere das zu testende PS-Flip-Flop
-  ps_ff_inst: ps_ff
-    port map (
-      d     => d_tb,
-      clk   => clk_tb,
-      Q     => Q_tb,
-      not_Q => not_Q_tb
-    );
+  ps_ff_inst : ps_ff
+  port map
+  (
+    d     => d,
+    clk   => clk,
+    Q     => Q,
+    not_Q => not_Q
+  );
 
   -- Testprozess
   process
   begin
     -- Initialwerte
-    d_tb <= '0';
-    clk_tb <= '0';
+    d   <= '0';
+    clk <= '0';
     wait for 10 ns;
 
     -- zu testende Kombinationen von d und clk
-    d_tb <= '1';
+    d <= '1';
     wait for 10 ns;
-    clk_tb <= '1';
+    clk <= '1';
     wait for 10 ns;
-    clk_tb <= '0';
-    wait for 10 ns;
-
-    d_tb <= '0';
-    wait for 10 ns;
-    clk_tb <= '1';
-    wait for 10 ns;
-    clk_tb <= '0';
+    clk <= '0';
     wait for 10 ns;
 
-    d_tb <= '1';
+    d <= '0';
     wait for 10 ns;
-    clk_tb <= '1';
+    clk <= '1';
     wait for 10 ns;
-    clk_tb <= '0';
+    clk <= '0';
+    wait for 10 ns;
+
+    d <= '1';
+    wait for 10 ns;
+    clk <= '1';
+    wait for 10 ns;
+    clk <= '0';
     wait for 10 ns;
 
     wait;
