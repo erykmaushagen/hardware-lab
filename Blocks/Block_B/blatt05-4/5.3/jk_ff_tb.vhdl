fb029af86334f56778c873c7c228628bfbc7d868
erykmaushagen <eryk.maushagen@gmail.com>
2025-11-22 23:40:50 +0100

    5.3 ready

--- Blocks/Block_B/blatt05-4/5.3/jk_ff_tb.vhdl
+++ Blocks/Block_B/blatt05-4/5.3/jk_ff_tb.vhdl
@@ -1,75 +1,83 @@
 library ieee;
-  use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all;
 
 entity jk_ff_tb is
 end entity;
 
 architecture testbench of jk_ff_tb is
-
   component jk_flipflop is
     port (
-      J, K, clk, reset : in  std_logic;
+      J, K, clk, reset : in std_logic;
       Q                : out std_logic
     );
   end component;
 
-  -- signaldeklaration mit defaultwerten
+  -- Signale für Testbench
   signal J, K, clk, reset : std_logic := '0';
   signal Q                : std_logic;
-
 begin
 
-  -- Instanziere das zu testende JK-Flip-Flop
-  jk_ff_inst: jk_flipflop
-    port map (
-      J     => J,
-      K     => K,
-      clk   => clk,
-      reset => reset,
-      Q     => Q
-    );
+  -- Instanziierung des Flip-Flops
+  uut : jk_flipflop
+  port map
+  (
+    J     => J,
+    K     => K,
+    clk   => clk,
+    reset => reset,
+    Q     => Q
+  );
 
-  -- Taktprozess der den Takt generiert und parallel zum Testprozess läuft
-  clk_process: process
-  begin
-    clk <= '0';
-    wait for 10 ns;
-    clk <= '1';
-    wait for 10 ns;
-  end process;
-
-  -- Testprozess der die Eingangsvariablen setzt
-  testing: process
+  -- Alles in einem Prozess
+  process
   begin
 
-    --
-    clk <= '0';
-    wait for 10 ns;
-
+    reset <= '0';
+    -- Taktzyklus 1: J=1, K=0 (Set) vor Takt setzen
+    J <= '1';
+    K <= '0';
+    wait for 5 ns; -- Signal leicht vor dem Takt
     clk <= '1';
     wait for 10 ns;
-
     clk <= '0';
-    wait for 10 ns;
+    wait for 5 ns;
 
+    -- Taktzyklus 2: J=0, K=1 (Reset) vor Takt setzen
+    J <= '0';
+    K <= '1';
+    wait for 5 ns;
     clk <= '1';
     wait for 10 ns;
-
     clk <= '0';
-    wait for 10 ns;
+    wait for 5 ns;
 
+    -- Taktzyklus 3: J=1, K=1 (Toggle) vor Takt setzen
+    J <= '1';
+    K <= '1';
+    wait for 5 ns;
     clk <= '1';
     wait for 10 ns;
-
     clk <= '0';
-    wait for 10 ns;
+    wait for 5 ns;
 
+    -- Taktzyklus 4: J=0, K=0 (No change) vor Takt setzen
+    J <= '0';
+    K <= '0';
+    wait for 5 ns;
     clk <= '1';
     wait for 10 ns;
-
     clk <= '0';
-    wait for 10 ns;
+    wait for 5 ns;
 
+    -- Signal aktivieren für Reset
+    reset <= '1';
+    wait for 5 ns;
+    clk <= '1';
+    wait for 10 ns;
+    clk <= '0';
+    wait for 5 ns;
+
+    wait;
   end process;
 
 end architecture;
