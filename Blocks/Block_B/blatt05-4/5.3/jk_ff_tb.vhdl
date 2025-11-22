bc786cc12c7c0d7005fc8bd91b056aa08b2af123
Eryk Maushagen <eryk.maushagen@gmail.com>
2025-11-22 19:35:03 +0100

    5.3 ready

--- Blocks/Block_B/blatt05-4/5.3/jk_ff_tb.vhdl
+++ Blocks/Block_B/blatt05-4/5.3/jk_ff_tb.vhdl
@@ -1,34 +1,78 @@
 library ieee;
-use ieee.std_logic_1164.all;
+  use ieee.std_logic_1164.all;
 
 entity jk_ff_tb is
-end jk_ff_tb;
+end entity;
 
 architecture testbench of jk_ff_tb is
 
-component JK_FlipFlop is
+  component JK_FlipFlop is
     port (
-      J,K, clk , reset : in std_logic;
-      Q : out std_logic
+      J, K, clk, reset : in  std_logic;
+      Q                : out std_logic
     );
-end component;
+  end component;
 
-
---missing
+  -- signaldeklaration mit defaultwerten
+  signal J, K, clk, reset : std_logic := '0';
+  signal Q                : std_logic;
 
 begin
 
---missing
+  -- Instanziere das zu testende JK-Flip-Flop
+  jk_ff_inst: JK_FlipFlop
+    port map (
+      J     => J,
+      K     => K,
+      clk   => clk,
+      reset => reset,
+      Q     => Q
+    );
 
-process begin
+  -- Taktprozess der den Takt generiert und parallel zum Testprozess läuft
+  clk_process: process
+  begin
+    clk <= '0';
+    wait for 10 ns;
+    clk <= '1';
+    wait for 10 ns;
+  end process;
 
---missing
-	
-	
-	
-	
-	wait;
-end process;
+  -- Testprozess der die Eingangsvariablen setzt
+  testing: process
+  begin
 
-end testbench;
+    -- J,K = 0,0  (Halten)
+    J <= '0';
+    K <= '0';
+    wait for 25 ns;
+
+    -- J,K = 0,1 (Reset)
+    J <= '0';
+    K <= '1';
+    wait for 25 ns;
+
+    -- J,K = 1,0 (Set)
+    J <= '1';
+    K <= '0';
+    wait for 25 ns;
+
+    -- J,K = 1,1 (Toggle)
+    J <= '1';
+    K <= '1';
+    wait for 25 ns;
+
+    -- noch ein Toggle
+    J <= '1';
+    K <= '1';
+    wait for 25 ns;
+
+    -- Reset
+    reset <= '1';
+    wait for 20 ns;
+
+    wait;
+  end process;
+
+end architecture;
 
