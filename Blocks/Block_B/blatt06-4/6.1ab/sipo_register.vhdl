660e9ddbb86993eb2d64495820dd6c0a4d38b533
Eryk Maushagen <eryk.maushagen@gmail.com>
2025-11-28 16:33:39 +0100

    6.1.a

--- Blocks/Block_B/blatt06-4/6.1ab/sipo_register.vhdl
+++ Blocks/Block_B/blatt06-4/6.1ab/sipo_register.vhdl
@@ -1,26 +1,56 @@
 library ieee;
-use ieee.std_logic_1164.all;
+  use ieee.std_logic_1164.all;
 
--- ein serieler Input, 8 bit parallel Output
+  -- ein serieler Input, 8 bit parallel Output
+
 entity sipo_register is
-    port(
-        serial_in, clear, clock : in std_logic;
-        Q : out std_logic_vector(3 downto 0)
-    );
-end sipo_register;
+  port (
+    serial_in, clear, clock : in  std_logic;
+    Q                       : out std_logic_vector(3 downto 0)
+  );
+end entity;
 
 architecture rtl of sipo_register is
-    component DFlipFlop is 
-        port (
-            d, clk, reset: in std_logic;
-            Q : out std_logic
-        );
-    end component;
+  component DFlipFlop is
+    port (
+      d, clk, reset : in  std_logic;
+      Q             : out std_logic
+    );
+  end component;
 
-    -- 
 begin
+  -- instanziierung des ersten flipflops
+  DFF1: DFlipFlop
+    port map (
+      d     => serial_in,
+      clk   => clock,
+      reset => clear,
+      Q     => Q(1)
+    );
+  -- instanziierung des zweiten flipflops
+  DFF2: DFlipFlop
+    port map (
+      d     => Q(1),
+      clk   => clock,
+      reset => clear,
+      Q     => Q(2)
+    );
+  -- instanziierung des dritten flipflops
+  DFF3: DFlipFlop
+    port map (
+      d     => Q(2),
+      clk   => clock,
+      reset => clear,
+      Q     => Q(3)
+    );
+  -- instanziierung des vierten flipflops
+  DFF4: DFlipFlop
+    port map (
+      d     => Q(3),
+      clk   => clock,
+      reset => clear,
+      Q     => ouQ(4)
+    );
 
-    --
-    
-end rtl;
+end architecture;
 
