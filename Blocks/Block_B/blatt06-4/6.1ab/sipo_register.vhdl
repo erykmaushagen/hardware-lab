ef4ecc91eb587fb26c8842c51b1bb6a8e62ac36d
erykmaushagen <eryk.maushagen@gmail.com>
2025-11-28 17:44:21 +0100

    6.1.ab

--- Blocks/Block_B/blatt06-4/6.1ab/sipo_register.vhdl
+++ Blocks/Block_B/blatt06-4/6.1ab/sipo_register.vhdl
@@ -1,11 +1,11 @@
 library ieee;
-  use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all;
 
-  -- ein serieler Input, 8 bit parallel Output
+-- ein serieler Input, 8 bit parallel Output
 
 entity sipo_register is
   port (
-    serial_in, clear, clock : in  std_logic;
+    serial_in, clear, clock : in std_logic;
     Q                       : out std_logic_vector(3 downto 0)
   );
 end entity;
@@ -13,44 +13,57 @@ end entity;
 architecture rtl of sipo_register is
   component DFlipFlop is
     port (
-      d, clk, reset : in  std_logic;
+      d, clk, reset : in std_logic;
       Q             : out std_logic
     );
   end component;
 
+  signal q0_out : std_logic;
+  signal q1_out : std_logic;
+  signal q2_out : std_logic;
+  signal q3_out : std_logic;
+
 begin
   -- instanziierung des ersten flipflops
-  DFF1: DFlipFlop
-    port map (
-      d     => serial_in,
-      clk   => clock,
-      reset => clear,
-      Q     => Q(0)
-    );
+  DFF1 : DFlipFlop
+  port map
+  (
+    d     => serial_in,
+    clk   => clock,
+    reset => clear,
+    Q     => q0_out
+  );
   -- instanziierung des zweiten flipflops
-  DFF2: DFlipFlop
-    port map (
-      d     => Q(0),
-      clk   => clock,
-      reset => clear,
-      Q     => Q(1)
-    );
+  DFF2 : DFlipFlop
+  port map
+  (
+    d     => q0_out,
+    clk   => clock,
+    reset => clear,
+    Q     => q1_out
+  );
   -- instanziierung des dritten flipflops
-  DFF3: DFlipFlop
-    port map (
-      d     => Q(1),
-      clk   => clock,
-      reset => clear,
-      Q     => Q(2)
-    );
+  DFF3 : DFlipFlop
+  port map
+  (
+    d     => q1_out,
+    clk   => clock,
+    reset => clear,
+    Q     => q2_out
+  );
   -- instanziierung des vierten flipflops
-  DFF4: DFlipFlop
-    port map (
-      d     => Q(2),
-      clk   => clock,
-      reset => clear,
-      Q     => Q(3)
-    );
+  DFF4 : DFlipFlop
+  port map
+  (
+    d     => q2_out,
+    clk   => clock,
+    reset => clear,
+    Q     => q3_out
+  );
+
+  Q(0) <= q0_out;
+  Q(1) <= q1_out;
+  Q(2) <= q2_out;
+  Q(3) <= q3_out;
 
 end architecture;
-
--- Blocks/Block_B/blatt06-4/6.1ab/sipo_register_tb.vhdl
+++ Blocks/Block_B/blatt06-4/6.1ab/sipo_register_tb.vhdl
@@ -1,55 +1,96 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity sipo_register_tb is 
+entity sipo_register_tb is
 end sipo_register_tb;
 
 architecture rtl of sipo_register_tb is
 
--- fehlt
-    
+  component sipo_register is
+    port (
+      serial_in, clear, clock : in std_logic;
+      Q                       : out std_logic_vector(3 downto 0)
+    );
+  end component;
+
+  -- definiere signale
+  signal serial_in : std_logic;
+  signal clear     : std_logic;
+  signal clock     : std_logic;
+  signal Q         : std_logic_vector(3 downto 0);
+
 begin
-   -- fehlt
+  -- fehlt
+  uut : sipo_register
+  port map
+  (
+    serial_in => serial_in,
+    clear     => clear,
+    clock     => clock,
+    Q         => Q
+  );
+  process begin
+    -- Ziel: serieller Eingang "1010" liegt parallel an Q1-Q4 zum 4.ten Takt
+    -- SIPO initialisieren indem man es zurueck setzt
+    clock     <= '0';
+    clear     <= '0';
+    serial_in <= '0';
+    wait for 10 ns;
+    --Zuruecksetzen.
 
-    process begin
-       -- SIPO initialisieren indem man es zurueck setzt
-        clock <= '0'; 
-        clear <= '0';
-        serial_data_input <= '0';
-        wait for 10 ns;
-        --Zuruecksetzen.
-        clear <= '1';
-        clock <= '1';
-        
-        wait for 10 ns;
-        clock <= '0'; 
-        clear <= '0';
-        wait for 10 ns;
+    clock <= '1';
 
-        --Uebergebe die Bitsequenz 1000 
-        --erstes Bit
-        serial_data_input <= '1';
-        clock <= '1';
-        wait for 10 ns;
-        clock <= '0'; 
-        wait for 10 ns; 
-        --weitere Bits
-        --vervollstandigen
-             
-        --Pause, nichts aendert sich
-        wait for 30 ns; 
-        --Zuruecksetzen.
-        clear <= '1';
-        clock <= '1';
-        serial_data_input <= '0';
-        wait for 10 ns;
-        clock <= '0'; 
-        clear <= '0';
-        wait for 10 ns;
-        
-        --Uebergebe weite Bitsequenz 
-        
-        wait;
-    end process; 
-    
-end rtl; 
+    wait for 10 ns;
+    clock <= '0';
+
+    wait for 10 ns;
+
+    --Uebergebe die Bitsequenz 1010 
+    --erstes Bit
+    serial_in <= '1';
+    clock     <= '1';
+    wait for 10 ns;
+
+    clock <= '0';
+    wait for 10 ns;
+    --weitere Bits
+    --vervollstandigen
+
+    -- zweiter Takt Übernahme des 2.ten Bits von "1010"
+    serial_in <= '0';
+    clock     <= '1';
+    wait for 10 ns;
+
+    clock <= '0';
+    wait for 10 ns;
+
+    -- dritter Takt Übernahme des 3.ten Bits aus ser. input "1010"
+    serial_in <= '1';
+    clock     <= '1';
+    wait for 10 ns;
+
+    clock <= '0';
+    wait for 10 ns;
+
+    -- vierter Takt Übernahme des 4.ten Bits aus ser. input "1010"
+    serial_in <= '0';
+    clock     <= '1';
+    wait for 10 ns;
+
+    clock <= '0';
+    wait for 10 ns;
+    -- => nach 4.tem takt sollte also paraller Output an Q1-Q4 liegen:
+    -- Q4 = 1; Q3 = 0; Q2 = 1; Q1 = 0
+
+    --Zuruecksetzen.
+    clear <= '1';
+    clock <= '1';
+    wait for 10 ns;
+
+    clock <= '0';
+    wait for 10 ns;
+
+    wait;
+  end process;
+
+end rtl;
