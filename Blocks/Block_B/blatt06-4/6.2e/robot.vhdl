bbb7febe0892c01aef52032fc4da3ca61456d304
erykmaushagen <eryk.maushagen@gmail.com>
2025-11-29 22:01:27 +0100

    6.2.e ready

--- Blocks/Block_B/blatt06-4/6.2e/robot.vhdl
+++ Blocks/Block_B/blatt06-4/6.2e/robot.vhdl
@@ -1,82 +1,34 @@
 library ieee;
-  use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all;
 
 entity robot is
   port (
-    pattern              : in  std_logic_vector(3 downto 0); -- das pattern das gefunden werden soll
-    sensor, reset, clock : in  std_logic;                    --sensor ist der aktuell gelesene Wert
+    pattern              : in std_logic_vector(3 downto 0); -- das pattern das gefunden werden soll
+    sensor, reset, clock : in std_logic; --sensor ist der aktuell gelesene Wert
     alarm                : out std_logic
   );
 end entity;
 
 architecture rtl of robot is
 
-  signal shiftreg : std_logic_vector(3 downto 0);
+  signal shiftreg    : std_logic_vector(3 downto 0);
+  signal pattern_cmp : std_logic_vector(3 downto 0);
 
 begin
-
-  --------------------------------------------------------------------
+  -- reverse logik
+  pattern_cmp <= pattern(0) & pattern(1) & pattern(2) & pattern(3);
   -- 4-Bit SISO-Schieberegister
-  --------------------------------------------------------------------
   process (clock, reset)
   begin
     if reset = '1' then
       shiftreg <= (others => '0');
     elsif rising_edge(clock) then
-      shiftreg <= shiftreg(2 downto 0) & sensor;
+      shiftreg <= shiftreg(2 downto 0) & sensor; -- aktuelle 4-Bit Maske relativ con sensor aus
     end if;
   end process;
 
-  --------------------------------------------------------------------
-  -- Patternvergleich
-  --------------------------------------------------------------------
-  alarm <= '1' when shiftreg = pattern else '0';
+  -- Patternvergleich der unser output determiniert
+  alarm <= '1' when shiftreg = pattern_cmp else
+    '0';
 
 end architecture;
-
--- architecture rtl of robot is
---   --missing
---   component piso_register is
---     port (
---       pinput : in  std_logic_vector(3 downto 0);
---       Clk    : in  std_logic;
---       clear  : in  std_logic;
---       write  : in  std_logic; -- write auf 1: es kann geschrieben werden
---       sout   : out std_logic
---     );
---   end component;
-
---   signal shiftin        : std_logic;
---   signal reload_pattern : std_logic;
-
--- begin
---   -- Serial In Parallel Out Register
---   pr: piso_register
---     port map (
---       pinput => pattern,
---       Clk    => clock,
---       clear  => reset,
---       write  => reload_pattern, -- wenn alarm auf 1 ist wird das pattern in das register geschrieben
---       sout   => shiftin
---     );
-
---   process (Clk, pinput)
---     variable counter : integer := pinput'length - 1;
---   begin
---     if (rising_edge(Clk)) then -- fuer jede steigende Taktflanke wird ein Element der Eingabesequenz abgegangen
---       if (counter >= 0) then
---         if (sensor = shiftin) then
---           counter := counter - 1;
---         else
---           counter := pinput'length - 1; -- wenn ein falsches bit gelesen wurde wird der counter zurueckgesetzt
---           clear <= '1';
---           reload_pattern <= '1'; -- pattern wird neu in das register geladen
-
---         end if;
---       end if;
---       if (counter = 0) then
---         alarm <= '1'; -- pattern wurde erkannt
---       end if;
---     end if;
---   end process;
--- end architecture;
