a62663642f4abf308e1e2b304a93d170701bc367
erykmaushagen <eryk.maushagen@tu-dortmund.de>
2025-12-07 13:18:00 +0100

    7.1.a ready

--- Blocks/Block_B/blatt07-4/7.1/parallel_mult.vhdl
+++ Blocks/Block_B/blatt07-4/7.1/parallel_mult.vhdl
@@ -13,7 +13,11 @@ entity parallel_mult is
 end entity;
 
 architecture rtl of parallel_mult is
-  signal --missing;
+  signal res_0_0, res_0_1, res_0_2, res_0_3 : std_logic;
+  signal res_1_0, res_1_1, res_1_2, res_1_3 : std_logic_vector(1 downto 0);
+  signal res_2_0, res_2_1, res_2_2, res_2_3 : std_logic_vector(1 downto 0);
+  signal res_3_0, res_3_1, res_3_2, res_3_3 : std_logic_vector(1 downto 0);
+
   component block_1
     port (
       andgate_in_1 : in  std_logic;
@@ -37,85 +41,84 @@ architecture rtl of parallel_mult is
   end component;
 
 begin
-  row_0_0: block_?
-    port map --missing;
-    row_0_1: block_? port map --missing;
-    row_0_2: block_? port map --missing;
-    row_0_3: block_? port map --missing;
-    row_1_0: block_? port map --missing;
-    row_1_1: block_? port map --missing;
-    row_1_2: block_? port map --missing;
-    row_1_3: block_? port map --missing;
-    row_2_0: block_? port map --missing;
-    row_2_1: block_? port map --missing;
-    row_2_2: block_? port map --missing;
-    row_2_3: block_? port map --missing;
-    row_3_0: block_? port map --missing; 
-    row_3_1: block_? port map --missing;
-    row_3_2: block_? port map --missing;
-    row_3_3: block_? port map --missing;
-    z(0) <= --missing;
-    z(1) <= --missing;
-    z(2) <= --missing;
-    z(3) <= --missing;
-    z(4) <= --missing;
-    z(5) <= --missing;
-    z(6) <= --missing;
-    z(7) <= --missing;
-  end ;
+  row_0_0: block_1 port map (andgate_in_1 => y(0), andgate_in_2 => x(0), andgate_out => res_0_0);
+  row_0_1: block_1 port map (andgate_in_1 => y(0), andgate_in_2 => x(1), andgate_out => res_0_1);
+  row_0_2: block_1 port map (andgate_in_1 => y(0), andgate_in_2 => x(2), andgate_out => res_0_2);
+  row_0_3: block_1 port map (andgate_in_1 => y(0), andgate_in_2 => x(3), andgate_out => res_0_3);
+  row_1_0: block_3 port map (a => res_0_1, b => x(0), c => y(1), cout => res_1_0(1), sum => res_1_0(0));
+  row_1_1: block_2 port map (a => res_0_2, b => x(1), c => y(1), cin => res_1_0(1), cout => res_1_1(1), sum => res_1_1(0));
+  row_1_2: block_2 port map (a => res_0_3, b => x(2), c => y(1), cin => res_1_1(1), cout => res_1_2(1), sum => res_1_2(0));
+  row_1_3: block_3 port map (a => res_1_2(1), b => x(3), c => y(1), cout => res_1_3(1), sum => res_1_3(0));
+  row_2_0: block_3 port map (a => res_1_1(0), b => x(0), c => y(2), cout => res_2_0(1), sum => res_2_0(0));
+  row_2_1: block_2 port map (a => res_1_2(0), b => x(1), c => y(2), cin => res_2_0(1), cout => res_2_1(1), sum => res_2_1(0));
+  row_2_2: block_2 port map (a => res_1_3(0), b => x(2), c => y(2), cin => res_2_1(1), cout => res_2_2(1), sum => res_2_2(0));
+  row_2_3: block_2 port map (a => res_1_3(1), b => x(3), c => y(2), cin => res_2_2(1), cout => res_2_3(1), sum => res_2_3(0));
+  row_3_0: block_3 port map (a => res_2_1(0), b => x(0), c => y(3), cout => res_3_0(1), sum => res_3_0(0));
+  row_3_1: block_2 port map (a => res_2_2(0), b => x(1), c => y(3), cin => res_3_0(1), cout => res_3_1(1), sum => res_3_1(0));
+  row_3_2: block_2 port map (a => res_2_3(0), b => x(2), c => y(3), cin => res_3_1(1), cout => res_3_2(1), sum => res_3_2(0));
+  row_3_3: block_3 port map (a => res_2_3(1), b => x(3), c => y(3), cout => res_3_3(1), sum => res_3_3(0));
+  z(0) <= res_0_0;
+  z(1) <= res_1_0(0);
+  z(2) <= res_2_0(0);
+  z(3) <= res_3_0(0);
+  z(4) <= res_3_1(0);
+  z(5) <= res_3_2(0);
+  z(6) <= res_3_3(0);
+  z(7) <= res_3_3(1);
+end architecture;
 
-  --first block
-  library ieee;
-    use ieee.std_logic_1164.all;
+--first block
+library ieee;
+  use ieee.std_logic_1164.all;
 
-  entity block_1 is
-    port (
-      andgate_in_1 : in  std_logic;
-      andgate_in_2 : in  std_logic;
-      andgate_out  : out std_logic
-    );
-  end entity;
+entity block_1 is
+  port (
+    andgate_in_1 : in  std_logic;
+    andgate_in_2 : in  std_logic;
+    andgate_out  : out std_logic
+  );
+end entity;
 
-  architecture rtl of block_1 is
-  begin
-    andgate_out <= andgate_in_1 and andgate_in_2;
-  end architecture;
+architecture rtl of block_1 is
+begin
+  andgate_out <= andgate_in_1 and andgate_in_2;
+end architecture;
 
-  --second block.
-  library ieee;
-    use ieee.std_logic_1164.all;
+--second block.
+library ieee;
+  use ieee.std_logic_1164.all;
 
-  entity block_2 is
-    port (
-      a, b, c, cin : in  std_logic;
-      cout, sum    : out std_logic
-    );
-  end entity;
+entity block_2 is
+  port (
+    a, b, c, cin : in  std_logic;
+    cout, sum    : out std_logic
+  );
+end entity;
 
-  architecture rtl of block_2 is
-    signal d : std_logic;
-  begin
-    d    <= b and c;
-    cout <= (a and d) or (a and cin) or (d and cin);
-    sum  <= a xor d xor cin;
-  end architecture;
+architecture rtl of block_2 is
+  signal d : std_logic;
+begin
+  d    <= b and c;
+  cout <= (a and d) or (a and cin) or (d and cin);
+  sum  <= a xor d xor cin;
+end architecture;
 
-  --third block.
-  library ieee;
-    use ieee.std_logic_1164.all;
+--third block.
+library ieee;
+  use ieee.std_logic_1164.all;
 
-  entity block_3 is
-    port (
-      a, b, c   : in  std_logic;
-      cout, sum : out std_logic
-    );
-  end entity;
+entity block_3 is
+  port (
+    a, b, c   : in  std_logic;
+    cout, sum : out std_logic
+  );
+end entity;
 
-  architecture rtl of block_3 is
-    signal d : std_logic;
-  begin
-    d    <= b and c;
-    sum  <= a xor d;
-    cout <= a and d;
-  end architecture;
+architecture rtl of block_3 is
+  signal d : std_logic;
+begin
+  d    <= b and c;
+  sum  <= a xor d;
+  cout <= a and d;
+end architecture;
 
